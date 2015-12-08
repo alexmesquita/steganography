08621c748133791cc0d944e9357326d8e4502e39
Álex Mesquita <alex.mesquita06@hotmail.com>
2015-12-08 18:23:14 -0200

    Get hash

--- src/stegagnography.c
+++ src/stegagnography.c
@@ -1,11 +1,14 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/*#define W 1028*/
-#define W 8
+#define W 1280
 #define H 720
-#define STEG_SIZE 3
+#define STEG_BYTES 2073600 /*Quantos bytes modificados no arquivo*/
+#define STEG_BITS 4 /*Quantos bits modificados cada byte tem*/
+
+int k = 0, row = 0, col = 0, key_col = 0, key_row = 0;
 
 int* get_key(char* path, int* key_size)
 {
@@ -36,10 +39,9 @@ int* get_key(char* path, int* key_size)
     return key;
 }
 
-
-char* get_steg_byts(char* path, int* key, int key_size, int steg_size)
+char* get_steg_bytes(char* path, int* key, int key_size, int steg_bytes)
 {
-    int i = 0, j = 0, row = 0, col = 0, steg_position = 0, file_position = 0, key_col = 0, key_row = 0;
+    int i = 0, file_position = 0, steg_position = 0;
     char read;
     char* bytes;
     FILE *steg_file;
@@ -51,25 +53,17 @@ char* get_steg_byts(char* path, int* key, int key_size, int steg_size)
         errx(1, "It was not possible to open the steg file");
     }
 
-    bytes = (char*) malloc(sizeof(char)*steg_size + 1);
+    bytes = (char*) malloc(sizeof(char)*steg_bytes + 1);
 
-    for (i = 0, j = 0; i < steg_size; ++i, ++j)
+    for (i = 0; i < steg_bytes; ++i, ++k)
     {
-        steg_position = key[j%key_size];
+        steg_position = key[k%key_size];
         
         if (steg_position)
         {
-            key_col = 0;
-            if (steg_position >=4 && steg_position <= 6)
-            {
-                key_col = 1;
-            }
-            else if (steg_position >=7 && steg_position <= 9)
-            {
-                key_col = 2;
-            }
+            key_col = (steg_position - 1) / 3;
 
-            key_row = (steg_position-1) % 3;
+            key_row = (steg_position - 1) % 3;
             file_position = (row + key_row) * W + col + key_col;
 
             fseek(steg_file, file_position, SEEK_SET);
@@ -90,26 +84,159 @@ char* get_steg_byts(char* path, int* key, int key_size, int steg_size)
             row += 3;
         }
     }
-    bytes[steg_size] = '\0';
     return bytes;
 }
+
+char* generate_steg(char* steg_bytes, int bytes_size, int steg_bits)
+{
+    int i, j, position, bit;
+    char* result_steg;
+    char aux = 0;
+
+    result_steg = (char*) malloc((sizeof(char) * bytes_size));
+
+    for (i = 0; i < bytes_size; ++i)
+    {
+        position = 1;
+        aux = 0;
+        for (j = 0; j < steg_bits; ++j)
+        {
+            bit = steg_bytes[i] & position;
+            position <<= 1;
+
+            switch (j % 5)
+            {
+                case 0:
+                    if(bit)
+                        aux |= 128;
+                    break;
+                case 1:
+                    if(bit)
+                        aux |= 64;
+                    break;
+                case 2:
+                    if(bit)
+                        aux |= 32;
+                    break;
+                case 3:
+                    if(bit)
+                        aux |= 16;
+                    break;
+                case 4:
+                    if(bit)
+                        aux |= 8;
+                    break;
+                default:
+                    errx(1, "Error when calculating the bits");
+                    break;
+            }
+        }
+        result_steg[i] = aux;
+    }
+    return result_steg;
+}
+
+
+char* generate_hash(char* hash_bytes, int bytes_size, int steg_bits)
+{
+    int i, j, h = 0, position, bit;
+    char* hash_steg;
+    char aux = 0;
+
+    hash_steg = (char*) malloc(sizeof(char) * 16);
+
+    for (i = 0; i < bytes_size; ++i)
+    {
+        position = 1;
+        for (j = 0; j < steg_bits && h < 16; ++j)
+        {
+            bit = hash_bytes[i] & position;
+            position <<= 1;
+
+            switch ((i*steg_bits + j) % 8)
+            {
+                case 0:
+                    if(bit)
+                        aux |= 128;
+                    break;
+                case 1:
+                    if(bit)
+                        aux |= 64;
+                    break;
+                case 2:
+                    if(bit)
+                        aux |= 32;
+                    break;
+                case 3:
+                    if(bit)
+                        aux |= 16;
+                    break;
+                case 4:
+                    if(bit)
+                        aux |= 8;
+                    break;
+                case 5:
+                    if(bit)
+                        aux |= 4;
+                    break;
+                case 6:
+                    if(bit)
+                        aux |= 2;
+                    break;
+                case 7:
+                    if(bit)
+                        aux |= 1;
+                    hash_steg[h] = aux;
+                    h++;
+                    aux = 0;
+                    break;
+                default:
+                    errx(1, "Error when calculating the bits");
+                    break;
+            }
+        }
+    }
+    return hash_steg;
+}
+
+
+void save_steg(char* steg, char* file, int size)
+{
+    FILE *steg_file;
+    steg_file = fopen(file, "w");
+    if (steg_file != NULL)
+    {
+        fwrite(steg , 1 , size, steg_file);
+    }
+    else
+    {
+        errx(1, "It was not possible to open the result file");
+    }
+}
+
 
 int main(int argc, char* argv[])
 {
     int* key;
     int key_size = 0;
-    char *steg;
+    char *steg_bytes, *result_steg, *hash_bytes, *hash_steg;
     switch (argc)
     {
         case 3:
             key = get_key(argv[2], &key_size);
-            steg = get_steg_byts(argv[1], key, key_size, STEG_SIZE);
+            steg_bytes = get_steg_bytes(argv[1], key, key_size, STEG_BYTES);
+            result_steg = generate_steg(steg_bytes, STEG_BYTES, STEG_BITS);
+            save_steg(result_steg, "res/result.y", STEG_BYTES);
+            hash_bytes = get_steg_bytes(argv[1], key, key_size, ceil(128.0 / STEG_BITS));
+            hash_steg = generate_hash(hash_bytes, ceil(128.0 / STEG_BITS), STEG_BITS);
+            save_steg(hash_steg, "res/hash", 16);
             break;
         default:
             errx(1, "invalid arguments");
     }
 
     free(key);
-    free(steg);
+    free(steg_bytes);
+    free(result_steg);
     return 0;
 }
